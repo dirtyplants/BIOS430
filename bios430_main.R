82f4f559947a9943f8d00fb0906b0cfc66235707
Plant Pants <dirtyplants@users.noreply.github.com>
2024-01-07 16:11:50 -0600

    Initial commit of materials for FA2024 BIOS430

---
+++ bios430_main.R
@@ -0,0 +1,53 @@
+# last modified August 24th, 2020.
+# created by karolis, modified by boris
+# evoswirl FA2020
+#
+# requires: https://igic.lab.uic.edu/courses/BIOS430/r/evoswirl_2020.08.27.zip (Windows binary)
+# requires: https://igic.lab.uic.edu/courses/BIOS430/r/evoswirl_2020.08.27.tgz (Mac binary)
+# requires: https://igic.lab.uic.edu/courses/BIOS430/r/evoswirl_2020.08.27.tar.gz (source)
+#
+install.missing <- function(packages, ...) {
+  new.packages <- packages[!(packages %in% installed.packages()[,'Package'])]
+  if(length(new.packages)) {
+    install.packages(new.packages, ...)
+  }
+}
+
+eswirl.version <- '2020.08.27'
+os.type <- as.character(Sys.info()['sysname'])
+
+if (os.type == 'Windows') {
+
+  writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = '~/.Renviron')
+  install.missing(c('devtools', 'httr', 'rvest', 'stringi', 'stringr', 'testthat', 'xml2', 'yaml', 'base64enc', 'RCurl', 'XML', 'ggplot2'), quiet=FALSE, verbose=TRUE, type='win.binary')
+  install.packages(paste0('https://igic.lab.uic.edu/courses/BIOS430/r/evoswirl_', eswirl.version, '.zip'), type='win.binary', repos=NULL, quiet=TRUE, verbose=FALSE)
+  # install.packages(paste0('https://igic.lab.uic.edu/courses/BIOS430/r/evoswirl_', eswirl.version, '.tar.gz'), type='source', repos=NULL, quiet=TRUE, verbose=FALSE)
+
+} else {
+
+  install.missing(c('devtools', 'httr', 'rvest', 'stringi', 'stringr', 'testthat', 'xml2', 'yaml', 'base64enc', 'RCurl', 'XML', 'ggplot2'), quiet=FALSE, verbose=TRUE)
+  install.packages(paste0('https://igic.lab.uic.edu/courses/BIOS430/r/evoswirl_', eswirl.version, '.tgz'), type='mac.binary', repos=NULL, quiet=TRUE, verbose=FALSE)
+  # install.packages(paste0('https://igic.lab.uic.edu/courses/BIOS430/r/evoswirl_', eswirl.version, '.tar.gz'), type='source', repos=NULL, quiet=TRUE, verbose=FALSE)
+
+}
+
+FILE_SEP <<- .Platform$file.sep
+HOME_DIR <<- normalizePath(path=Sys.getenv('HOME'))
+
+BIOS430_DIR <<- normalizePath(file.path(HOME_DIR, 'BIOS430', fsep=FILE_SEP), mustWork=FALSE)
+dir.create(BIOS430_DIR, recursive=TRUE, showWarnings=FALSE)
+
+setwd(BIOS430_DIR)
+
+SWIRL_DATA_DIR <<- normalizePath(file.path(BIOS430_DIR, 'Swirl', 'Data', fsep=FILE_SEP), mustWork=FALSE)
+dir.create(SWIRL_DATA_DIR, recursive=TRUE, showWarnings=FALSE)
+
+cat(paste0('\nYour class directory location is: ', BIOS430_DIR, '\n\n'))
+
+evoswirl::swirl_options(swirl_data_dir=SWIRL_DATA_DIR)
+
+cat('Looks like everything worked well, you are ready to begin!\n\n')
+
+cat('At the console prompt, first type:\n\n> library(evoswirl)\n\nto load the evoswirl package.\n\n')
+
+cat('After that, to begin evoswirl lessons, type:\n\n> swirl()\n\n')
